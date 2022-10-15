e62f0f3eb11de0c6c6945091fb3f5fe592753db3
Ivan Kornienko <ivkornienko2784@gmail.com>
2022-10-15 17:27:46 +0300

    Initial commit

---
+++ app/src/main/java/com/ivkornienko/asui/scanner/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.ivkornienko.asui.scanner
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
