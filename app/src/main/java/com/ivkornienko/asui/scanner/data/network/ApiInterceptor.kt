51d72c4ea3cd3f63c31a3fcbdb0e13f7368f810b
Ivan Kornienko <ivkornienko2784@gmail.com>
2022-10-22 15:52:10 +0300

    Use Dagger 2 (behind of Retrofit)

--- app/src/main/java/com/ivkornienko/asui/scanner/data/network/ApiInterceptor.kt
+++ app/src/main/java/com/ivkornienko/asui/scanner/data/network/ApiInterceptor.kt
@@ -1,10 +1,11 @@
 package com.ivkornienko.asui.scanner.data.network
 
+import com.ivkornienko.asui.scanner.domain.entity.ApiSettings
 import okhttp3.Credentials
 import okhttp3.Interceptor
 
-class BasicAuthInterceptor(username: String, password: String) : Interceptor {
-    private var credentials: String = Credentials.basic(username, password)
+class ApiInterceptor(settings: ApiSettings) : Interceptor {
+    private var credentials: String = Credentials.basic(settings.login, settings.password)
 
     override fun intercept(chain: Interceptor.Chain): okhttp3.Response {
         var request = chain.request()
